7b30a405e093303d5384a8e1c7cd075ff4e31183
Henry Allan <henryallan50@gmail.com>
2019-10-24 19:11:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,19 @@
 def reverse_each_word(sentence)
   
   array = sentence.split(" ")
+  count = 0
   
   array.collect do |word|
     
     new_word = word.reverse
     
     p new_word
+    
+    new_array[count] = new_word
+    
+    p new_array
+    
+    count += 1
     
   end
   
